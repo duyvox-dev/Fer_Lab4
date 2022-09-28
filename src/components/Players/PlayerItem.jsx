eaa3420662c0998b1011dd0e6a2c366a1862edd1
duyvox-dev <vobaoduy123@gmail.com>
2022-09-28 16:30:12 +0700

    add classname for playeritem

--- src/components/Players/PlayerItem.jsx
+++ src/components/Players/PlayerItem.jsx
@@ -4,8 +4,8 @@ export default function PlayerItem({ playerData }) {
     return (
         <div className="card">
             <img src={playerData.img} alt="" />
-            <h3>{playerData.name}</h3>
-            <p>{playerData.club}</p>
+            <h3 className="player-name">{playerData.name}</h3>
+            <p className="player-club">{playerData.club}</p>
             <Link to={`/detail/${playerData?.id}`}>Detail</Link>
         </div>
     );
